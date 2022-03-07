50bfe9a36ba9be22600960004fdf60d963f0b79b
simonmortensen23 <simon020393@gmail.com>
2022-03-07 09:46:40 +0000

    Overview of functions

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import 
+
+def newGame():
+
+def getWord():
+
+def playerGuess():
+
+def checkGuess():
+
+def addLetter():
+
+def playAgain():
+
+def endGame():
+
