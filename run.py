bb2bd48698b25069c34a1ba9247e8ec5bab9fb78
simonmortensen23 <simon020393@gmail.com>
2022-03-10 13:08:14 +0000

    add displayGame, playerGuess and game funciton

--- run.py
+++ run.py
@@ -2,38 +2,116 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
-from words import words
+
+
+
+lives = "hangman"
+words = ["orangutan", "message", "breakfast"]
 
 def getWord(wordList):
     """
     This function selects and returns a random word from the words.py file
     """
-    wordIndex = random.randint(0, len(wordIndex) - 1)
-    return wordList(wordIndex)
+    word = random.choice(words) #Chooses random word from words array
+    return word
 
-
-def newGame():
-    print("HANGMAN") 
-    missedLetters = ''
-    correctLetters = ''
-    unknownWord = getWord(words)
-    gameOver = False
-
+def displayGame(missedLetters, correctLetters, unknownWord):
+    """
+    This function prints the layout of the playerboard and updates it after every turn
+    """
+    playerLives = '_' * len(lives)
+    print(playerLives)
     
+    print(f"Player: {playerName} from {playerCity}")
+    print()
+    print(f"Letters you have guessed wrong so far: ", ' '.join(missedLetters))
+    print()
+    blanks = '_' * len(unknownWord) #Finds _ * the length of the unknown word
+    
+    for i in range(len(unknownWord)):
+        if unknownWord[i] in correctLetters:
+            blanks = blanks[:i] + unknownWord[i] + blanks[i+1:]
+    
+    for letter in blanks:
+        print(letter, end=' ')
+    print()
 
-#def getRandomWord():
+def playerGuess(alreadyGuessed):
+    """
+    Handles the player guess input and checks if it a single letter that has not been used yet
+    """
+    while True:
+        guess = input("Guess a letter: ")
+        print()
+        guess = guess.lower()
+        if len(guess) != 1:
+            print("Please enter a single letter.")
+        elif guess in alreadyGuessed:
+            print("You have already guessed that letter. Choose again.")
+        elif guess not in "abcdefghijklmnopqrstuvwxyz":
+            print("Please enter a LETTER")
+        else:
+            return guess
 
-#def playerInput():
+def playAgain():
+    """
+    This function asks the player if they want to play again or quit the game
+    """
+    play_again = input("Do you want to play again? (yes or no): ")
+    if play_again == "yes":
+        gameOver = False
+    else:
+        print("Game is over, enjoy your day!")
 
-#def checkPlayerInput():
 
-#def addScore():
+print("HANGMAN") 
+missedLetters = ''
+correctLetters = ''
+unknownWord = getWord(words)
+gameOver = False
+playerName = input("Enter your name: ")
+print()
+playerCity = input("Enter your city: ")
+print()
 
-#def playAgain():
 
-#def endGame():
+while True:
+    displayGame(missedLetters, correctLetters, unknownWord)
 
-newGame()
+    guess = playerGuess(missedLetters + correctLetters)
+    
+    
+    if guess in unknownWord:
+        correctLetters = correctLetters + guess
+
+        foundAllLetters = True
+        for i in range (len(unknownWord)):
+            if unknownWord[i] not in correctLetters:
+                foundAllLetters = False
+                break
+        if foundAllLetters:
+            print("Congratiulations! You have guessed the word: " + unknownWord)
+            gameOver = True
+    else:
+        missedLetters = missedLetters + guess
+
+        if len(missedLetters) == len(lives):
+            displayGame(missedLetters, correctLetters, unknownWord)
+            print("You are out of lives!\nAfter " + str(len(missedLetters)) + " missed guesses and " + str(len(correctLetters)) + " correct guesses, the word was '" + unknownWord + "'") 
+            gameOver = True
+
+    if gameOver:
+        if playAgain():
+            missedLetters = ''
+            correctLetters = ''
+            gameOver = False
+            secretWord = getRandomWord(words)
+        else:
+            break
+
+
+
+
 
 
    
